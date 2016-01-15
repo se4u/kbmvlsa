932bfdc8acfeccfa010e73552f90cb0680093b22
pushpendre <pushpendre@jhu.edu>
2016-01-14 19:44:50 -0500

    Fix the random walk code.

--- src/random_walk_on_igraph.r
+++ src/random_walk_on_igraph.r
@@ -1,12 +1,10 @@
 #!/usr/bin/env Rscript
-### the sampling step would need work
-### can run a bunch of these to get nMC higher
-### Can you put together a quick script that takes an i-graph object
-### set of starts, and walk length and nMC as its input and
-### 1.  runs nMC independent random walks starting at each of the start vertices
-### 2.  aggregates across all the runs, and computes the number of times each
+### This script takes an i-graph object a set of starts, walk length and nMC as input
+### It runs:
+### 1.  nMC independent random walks starting at each of the start vertices
+### 2.  Aggregates across all the runs, and computes the number of times each
 ###     state is visited across all runs
-### 3.  gives as its output a list of all vertices ranked according to how often
+### 3.  Gives as its output a list of all vertices ranked according to how often
 ###     they were visited across all the runs, along with the associated ranks
 ###     (so that ties can be seen)
 ### USAGE: R -q -e "source('random_walk_on_igraph.r'); main()"
@@ -14,18 +12,19 @@ library("igraph")
 
 RW_weighted <- function(g, start,
                         path.maxlength=c(10, 25, 50),
-                        n.runs=50){
+                        n.runs=50,
+                        attr.name="weights"){
     ## g is igraph object with weighted edges
     ## start is a vector of start nodes.
     ## if edges unweighted, use built in RW function
     visit=rep(NaN, length(V(g)))
-    for(run in 1:nruns){
+    for(run in 1:n.runs){
         for(i in 1:length(start)){
             state=start[i]
             steps=0
             while(steps < path.maxlength){
                 w = graph.strength(g, state)
-                A <- as_adjacency_matrix(g, attr="weight")[state,]
+                A <- as_adjacency_matrix(g, attr=attr.name)[state,]
                 state <- sample(1:length(visit), 1, replace=FALSE, prob=A/w)
                 steps = steps+1
                 visit[state] <- visit[state]+1
@@ -45,13 +44,18 @@ main <- function(
 
     max.fields = max(count.fields(query.fn))
     queries = read.table(query.fn, header=FALSE, fill=TRUE, col.names=1:max.fields)
+    n.queries = nrow(queries)
+    print(paste("n.queries =", n.queries))
     ## for query in queries
-    for(qid in 1:nrow){
+    for(qid in 1:n.queries){
         starting_points = queries[qid,-1][!is.na(queries[qid,-1])]
+        n.starting_points = length(starting_points)
         ## for start in start_nodes
-        for(starting_point_idx in 1:length(starting_points)){
+        print(paste("n.starting_points =", n.starting_points))
+        for(starting_point_idx in 1:n.starting_points){
             start = starting_points[starting_point_idx]
             ## store a vector of counts for each start into a df
+            print(paste(qid, starting_point_idx))
             RW_weighted(graph, start, path.maxlength=path.maxlength, n.runs=n.runs)
         }
     }
