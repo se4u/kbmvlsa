e9577ae73143931384154691d9cd7d036a018a2a
pushpendre <pushpendre@jhu.edu>
2016-01-21 03:53:58 -0500

    Commit Kelvin Code

--- src/VN1.R
+++ src/VN1.R
@@ -2,154 +2,120 @@
 ### This is an easy to use R implementation of one of our VN procedures
 ### The code first embeds the graph into a suitable Euclidean space and then
 ### runs a suitable classifier with regression to get the nomination procedure
-### USAGE: R -q -e "source('VN1.R'); VN1()"
+### USAGE: R -q -e "source('VN1.R'); main(maxd=30)"
 ### dependencies
 library("igraph")
 library("mclust")
 library("irlba")
+library("Matrix")
+source("classifier.r")
 if("ssClust" %in% rownames(installed.packages()) == TRUE){
     ## line to include ssCLUST
     require(ssClust,lib.loc="/home/hltcoe/vlyzinski/...")
 }
 
-VN1<-function(
-    g=NaN, knownRed=c(), knownNotRed=c(), maxd=500,
-    ProjectToSphere=FALSE, Laplacian=FALSE, classifier="logistic"){
-    ## g is an igraph graph object
-    ## check if graph has edge weights and extract the
-    ## adjacency matrix of the graph
-    ## classifier is:
-    ## "ssgmm" for SS GMM (slow and great);
-    ## "kmean" for Unsupervised K-means (fast and dirty);
-    ## "randomforest" for Random Forest;
-    ## "logistic" Logisitic regression
-    print("Started VN1")
+main <- function(
+     graph_fn="/home/hltcoe/ngao/miniScale-2016/Kelvin/BinaryCoMentionGraph",
+     vertex_feature_save_fn="/export/projects/prastogi/kbvn/Kelvin.BinaryCoMentionGraph.features",
+     vertex_name_save_fn="/export/projects/prastogi/kbvn/Kelvin.BinaryCoMentionGraph.vertexnames",
+     maxd=100,
+     Laplacian=FALSE,
+     nElbows=2,
+     dim_override=1, save.vertex.names=FALSE){
+    tic = proc.time()[3] 
+    g <- read.graph(graph_fn, format="ncol", directed=FALSE)
+    print(paste("Time taken for reading graph=", proc.time()[3] - tic)) # 45s
+    vertex.names=V(g)$name    
+    # Save the names of the vertices.
+    if(save.vertex.names){
+    	save("vertex.names", file=vertex_name_save_fn)
+    }
+    ## V = 270,261
+    ## E = 12,526,506
+    print(paste("Number of Vertices=", length(V(g)), " Number of Edges=", ecount(g)))
+    ## vertex_attr(g, "name", index=10000) ":e_0b52856ecffd3d8616d3a30f676a50ad_40"
+    tic = proc.time()[3] 
+    w = edge_attr(g, "weight")
+    print(paste("Time taken for getting weights=", proc.time()[3] - tic))
 
-    if(is.nan(g)){
-        g = graph.atlas(1000)
-        g.was.nan = TRUE
-        knownRed = c(1,7)
-        knownNotRed = c(3,4)
-        print("The graph was Nan. Initialized graph g= ")
-        print(g)
-    } else {
-        g.was.nan = FALSE
-    }
+    print(paste("Maximum edge weight=", max(w)))
+    tic = proc.time()[3] 
+    dd <- graph.strength(
+       g, vids=V(g), loops=FALSE) # Putting weights="weight" also causes error
+    d = unname(dd)
+    print(paste("Time taken for graph.strength=", proc.time()[3] - tic))
 
-    if(length(edge_attr(g)$weight)==0){
-        A<-as_adjacency_matrix(g)
-        d<-degree(g)
-    }else{
-        A<-as_adjacency_matrix(g, attr="weight")
-        d<-edge_attr(g)$weight
-    }
-    if(g.was.nan){
-        print(A)
-        print(d)
-    }
+    tic = proc.time()[3] 
+    A <- as_adjacency_matrix(g, attr="weight", edges=FALSE, names=FALSE, sparse=TRUE)
+    print(paste("Time taken for getting adjacency=", proc.time()[3] - tic))
+
     maxd = min(maxd, min(nrow(A), ncol(A)) - 1)
-    print("maxd=")
-    print(maxd)
-    ## we extracted the edge list, now we embed the graph
-    ## maxd is the maximum number of singular vectors you can
-    ## compute, then we use an elbow finder to find a suitable
-    ## dimension for the embedding
-    ## set Laplacian=TRUE if you want to do Laplacian spectral embedding,
-    ## otherwise, adjacency matrix is embedded directly
+    print(paste("maxd=", maxd)) # max(198489)
+    tic = proc.time()[3] 
     if(Laplacian==TRUE){
-        D<-Diagonal(length(V(g)),d^{-1/2})
-        A=D%*%A%*%D
-        A.svd <- irlba(A, nu = maxd, nv = maxd)
-        dim=getElbows(A.svd$d,2)[2]
-        A.svd.values <- A.svd$d[1:dim]
-        A.svd.vectors <- A.svd$v[,1:dim]
-        A.coords <- A.svd.vectors
+        print("Doing Laplacian")
+        A.svd = embed.graph.laplacian(A, d, maxd, nElbows)
+	print(A.svd$d)
+	dim=min(maxd, max(dim_override, getElbows(A.svd$d, nElbows)[2]))
+	vertex.features = A.svd$v[,1:dim]
     }else{
-        D<-Diagonal(length(V(g)),d/length(V(g)))
-        A<-A+D
-        A.svd <- irlba(A, nu = maxd, nv = maxd)
-        dim=getElbows(A.svd$d,2)[2]
-        A.svd.values <- A.svd$d[1:dim]
-        A.svd.vectors <- A.svd$v[,1:dim]
-        A.coords <- A.svd.vectors %*% diag(sqrt(A.svd.values))
+        print("Doing Adjacency")
+        A.svd = embed.graph.adjacency(A, d, maxd, nElbows)
+	print(A.svd$d)
+	dim=min(maxd, max(dim_override, getElbows(A.svd$d, nElbows)[2]))
+	vertex.features = ((A.svd$v[,1:dim]) %*% diag(sqrt(A.svd$d[1:dim])))
     }
-
-    ## if the graph is very sparse, you may want to project the embedding to the sphere
-    ## in order to mitigate sparcity concerns
-    if(ProjectToSphere==TRUE){
-        for(i in 1:dim(A.coords)[1])
-            {A.coords[i,]<-A.coords[i,]/(sum(A.coords[i,]^2)^(1/2))}
+    print(paste("Time taken for embedding graph=", proc.time()[3] - tic))
+    print(paste("Dimensions kept=", dim))
+    ##--------------------#
+    ## Save Graph Coords. #
+    ##--------------------#
+    out_fn = paste(vertex_feature_save_fn, "_maxd~", maxd, "_Laplacian~", Laplacian, '_SVD', sep='')
+    print(out_fn)
+    save("A.svd", file=out_fn)
+    ## Save vertex.features
+    out_fn = paste(vertex_feature_save_fn, "_maxd~", maxd, "_ProjectToSphere~FALSE", 
+                   "_Laplacian~", Laplacian, "_dimkept~", dim, sep='')
+    print(out_fn)
+    save("vertex.features", file=out_fn)
+    ## Project to Sphere and Save vertex features.
+    for(i in 1:dim(vertex.features)[1]){
+        vertex.features[i,] <- vertex.features[i,]/(sum(vertex.features[i,]^2)^(1/2))
     }
-
-    ## run the classifier
-    ## if not so big, use ssMclust
-    if(classifier == "ssgmm"){
-        n=nrow(A.coords)
-        trueLabels <- rep(NA, n)
-        trueLabels[knownRed] = 1
-        knownCannotLink = knownNotRed
-        cannotLinkWithIdx=new.env()
-        for(i in knownCannotLink){
-            cannotLinkWithIdx[[as.character(i)]]<-knownRed[1]
-        }
-        ss<-ssClust(X = A.coords,
-                    knownLabels=knownRed,
-                    trueLabels=trueLabels,
-                    knownCannotLink=knownNotRed,
-                    cannotLinkWithIdx=cannotLinkWithIdx,
-                    Grange=c(2:5),
-                    modelNames=c('VVV',"EEE","VII","EII"),
-                    runParallel=F,
-                    fracOfCores2Use=1,
-                    initClassAssignments=NULL,
-                    initializationStrategy = "kpp")
-
-        ##now determine which class is "red"
-        redClassNumber <- unique(ss$cl[knownRed])
-
-        ##then process the z's and get mahalanobis distance for rankings
-        redRanking <- ss$z[,redClassNumber]
-        NominationScheme <- order(redRanking, decreasing = T)
-
-    }else if(classifier == "kmeans"){
-        X<-kmeans(A.coords,dim)
-    }else if(classifier == "randomforest"){
-        ## Create a random forest classifier.
-        stop(paste("Not Implemented classifier: ", classifier))
-    } else if(classifier == "logistic"){
-        ## Create a logistic regression classifier.
-        ## Perform the classification.
-        ## Rank the outputs by class probabilities.
-        labels = rep(NA, nrow(A.coords))
-        labels[knownRed] = 1
-        labels[knownNotRed] = 0
-        source("classifier.r")
-        retval = my.classify.wrapper(
-            as.data.frame(A.coords),
-            data.frame(y=labels),
-            glm.families = list(binomial(link='logit')),
-            return.everything=TRUE)
-        print("Fit the model")
-        model = retval$model
-        train.data = retval$train.data
-        test.data = retval$test.data
-        predict.fn = retval$predict.fn
-        train.prediction = predict.fn(model, train.data)
-        train.error = get.error(model, train.data, train.prediction)
-        print(train.error)
-        ## test.prediction = predict.fn(model, test.data)
-        ## test.error = get.error(model, test.data, test.prediction)
-    } else {
-        stop(paste("Not Implemented classifier: ", classifier))
-    }
+    out_fn = paste(vertex_feature_save_fn, "_maxd~", maxd, "_ProjectToSphere~TRUE", 
+                   "_Laplacian~", Laplacian,  "_dimkept~", dim,  sep='')
+    print(out_fn)
+    save("vertex.features", file=out_fn)
 }
 
-get.error <- function(model, data, prediction){
-    prediction = predict(model, newdata = data, type="response")
-    return(prediction == data$y)
+embed.graph.laplacian <- function(A, d, maxd, nElbows=2){
+    D <- Diagonal(nrow(A), d^{-1/2})
+    A = D %*% A %*% D
+    A.svd <- irlba(A, nv = maxd, right_only=TRUE, verbose=TRUE, maxit=100)
+    return(A.svd)
 }
+
+embed.graph.adjacency <- function(A, d, maxd, nElbows=2){
+    D <- Diagonal(nrow(A), d/nrow(A))
+    A <- A+D
+    A.svd <- irlba(A, nv = maxd, right_only=TRUE, verbose=TRUE, maxit=100)
+    return(A.svd)
+}
+
+embed.graph <- function(A, d, maxd, Laplacian, nElbows=2){
+    ## A - The adjacency/laplacian matrix.
+    ## d - The graph vertex strengths.
+    ## maxd - the dimension extracted from SVD
+    ## nElbows - The elbows kept after elbow finding.
+    ## Laplacian flag determines the way to embed A (rather its interpretation)
+    ## Projecting the embedding to the sphere mitigates
+    ## sparcity concerns when the graph is very sparse.
+    
+    return( vertex.features)
+}
+
+
 ## the elbow finding function
-
 getElbows <- function(dat, n = 3, threshold = FALSE, plot = TRUE) {
     ## Given a decreasingly sorted vector, return the given number of elbows
     ##
