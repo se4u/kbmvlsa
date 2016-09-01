42fd21711be0c95a43c16d8bc33849da52dfb025
pushpendre <pushpendre@jhu.edu>
2016-09-01 13:06:38 -0400

    add argparser options to cat to count file.

--- src/humane_rec/wikilink_category_to_count.py
+++ src/humane_rec/wikilink_category_to_count.py
@@ -4,19 +4,24 @@
 | Description :
 | Author      : Pushpendre Rastogi
 | Created     : Thu Sep  1 12:34:31 2016 (-0400)
-| Last-Updated: Thu Sep  1 12:55:13 2016 (-0400)
+| Last-Updated: Thu Sep  1 13:05:00 2016 (-0400)
 |           By: Pushpendre Rastogi
-|     Update #: 4
+|     Update #: 7
 '''
 import cPickle as pkl
 import rasengan
 from wikilink_category_to_url_and_count_reverse_index import WikilinkReverseIndex
 import sys
+import argparse
+arg_parser = argparse.ArgumentParser(description='')
+arg_parser.add_argument('--in_pkl_fn', default='data/wikilink_category_to_url_and_count_reverse_index.pkl', type=str)
+arg_parser.add_argument('--out_fn', default='data/wikilink_category_to_count.tsv', type=str)
+arg_parser.add_argument('--mention_thresh', default=10, type=int)
+args=arg_parser.parse_args()
 with rasengan.tictoc('loading pkl', override='stderr'):
-    wri = pkl.load(open(sys.argv[1]))
-for (a, b) in wri.iteritems():
-    print '%s\t%d\t%d\t%s' % (
-        a,
-        len(b),
-        sum([e[1] for e in b]),
-        '\t'.join(str(e[1]) for e in b))
+    wri = pkl.load(open(args.in_pkl_fn))
+with open(args.out_fn, 'wb') as f:
+    for (a, b) in wri.iteritems():
+        b = [e for e in b if e[1] >= args.mention_thresh]
+        f.write('%s\t%d\t%d\t%s\n' % (
+            a, len(b), sum([e[1] for e in b]), ' '.join(str(e[1]) for e in b)))
