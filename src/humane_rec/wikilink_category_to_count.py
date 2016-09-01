0269310badf1c31072d1f5bb90d749ecf9c9c367
Pushpendre Rastogi <pushpendre@gmail.com>
2016-09-01 12:56:52 -0400

    Print the time taken to stderr.

--- src/humane_rec/wikilink_category_to_count.py
+++ src/humane_rec/wikilink_category_to_count.py
@@ -4,15 +4,15 @@
 | Description :
 | Author      : Pushpendre Rastogi
 | Created     : Thu Sep  1 12:34:31 2016 (-0400)
-| Last-Updated: Thu Sep  1 12:40:23 2016 (-0400)
+| Last-Updated: Thu Sep  1 12:55:13 2016 (-0400)
 |           By: Pushpendre Rastogi
-|     Update #: 3
+|     Update #: 4
 '''
 import cPickle as pkl
 import rasengan
 from wikilink_category_to_url_and_count_reverse_index import WikilinkReverseIndex
 import sys
-with rasengan.tictoc('loading pkl'):
+with rasengan.tictoc('loading pkl', override='stderr'):
     wri = pkl.load(open(sys.argv[1]))
 for (a, b) in wri.iteritems():
     print '%s\t%d\t%d\t%s' % (
