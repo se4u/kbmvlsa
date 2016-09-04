2b48254ebab693dcf08dd46f8f13d2c7bfd90e5b
Pushpendre Rastogi <pushpendre@gmail.com>
2016-09-04 15:43:55 -0400

    update repo with code to extract cat-person mentions.

--- src/humane_rec/chosen_url.py
+++ src/humane_rec/chosen_url.py
@@ -4,9 +4,9 @@
 | Description : Print the chosen urls into a single file based on chosen categories.
 | Author      : Pushpendre Rastogi
 | Created     : Mon Aug 29 18:35:44 2016 (-0400)
-| Last-Updated: Thu Sep  1 12:19:33 2016 (-0400)
+| Last-Updated: Thu Sep  1 13:42:50 2016 (-0400)
 |           By: Pushpendre Rastogi
-|     Update #: 8
+|     Update #: 10
 '''
 import cPickle as pkl
 from wikilink_category_to_url_and_count_reverse_index import WikilinkReverseIndex
@@ -18,12 +18,16 @@ arg_parser.add_argument(
     '--caturl_pkl', default='data/wikilink_category_to_url_and_count_reverse_index.pkl', type=str)
 arg_parser.add_argument(
     '--chosen_cat', default='data/chosen_wikilink_categories', type=str)
+arg_parser.add_argument('--mention_thresh', default=10, type=int)
 args = arg_parser.parse_args()
 chosen_cat = set([e.strip().split()[0]
                   for e in open(args.chosen_cat) if e != '\n'])
-with rasengan.tictoc('loading pkl'):
+with rasengan.tictoc('loading pkl', override='stderr'):
     caturl = pkl.load(open(args.caturl_pkl))
-for cat in chosen_cat:
+for row in open(args.chosen_cat):
+    if row == '\n':
+        continue
+    cat = row.strip().split()[0]
     for url, cnt in caturl[cat]:
-        if cnt > 10:
+        if cnt >= args.mention_thresh:
             print cat, url
