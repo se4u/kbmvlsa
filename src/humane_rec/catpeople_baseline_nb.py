048bf3edac52c8cec0cc1424c1656b9ee0476391
Pushpendre Rastogi <pushpendre@gmail.com>
2016-09-19 00:36:51 -0400

    Add the new metrics. Commit before refactor.

--- src/humane_rec/catpeople_baseline_nb.py
+++ src/humane_rec/catpeople_baseline_nb.py
@@ -4,9 +4,9 @@
 | Description : The Baseline NB experiment.
 | Author      : Pushpendre Rastogi
 | Created     : Sun Sep  4 18:32:35 2016 (-0400)
-| Last-Updated: Sun Sep 11 21:06:48 2016 (-0400)
+| Last-Updated: Mon Sep 19 00:34:26 2016 (-0400)
 |           By: Pushpendre Rastogi
-|     Update #: 269
+|     Update #: 282
 This script implements the NB baseline described in Section 3.1 of the humane_rec paper.
 '''
 import random
@@ -220,28 +220,28 @@ class Performance_Aggregator(object):
         for _, i in fold[2]:
             arr[i] = 1
         ap = average_precision(arr)
+        # mean_reciprocal_rank(arr)
+        mrr = 1.0 / (1 + min(e[1] for e in fold[2]))
         p_at_10 = float(sum(arr[:10]))
         p_at_100 = float(sum(arr[:100]))
         random.shuffle(arr)
         rap = average_precision(arr)
+        # The Random MRR can be deterministically computed.
+        rmrr = 1.0 / (1.0 + float(len(fold[1])) / (len(fold[2]) + 1))
         rp_at_10 = float(sum(arr[:10]))
         rp_at_100 = float(sum(arr[:100]))
-        return [ap, rap, p_at_10, rp_at_10, p_at_100, rp_at_100]
+        return [ap, rap, p_at_10, rp_at_10, p_at_100, rp_at_100, mrr, rmrr]
 
     def report_fold(self, cat, fold, S_size=-1):
-        [ap, rap, p_at_10, rp_at_10, p_at_100,
-            rp_at_100] = self.get_fold_stats(fold)
-        return '%-40s(Training=%-3d,needles=%-3d,haystack=%-3d) (AUPR %.3f %.3f) (P@10 %d %d) (P@100 %d %d)' % (
-            cat, S_size, len(fold[0]), len(fold[1]), ap, rap, p_at_10, rp_at_10, p_at_100, rp_at_100)
+        return '%-40s(Training=%-3d,needles=%-3d,haystack=%-3d) (AUPR %.3f %.3f) (P@10 %d %d) (P@100 %d %d) (MRR %.3f %.3f)' % tuple(
+            [cat, S_size, len(fold[0]), len(fold[1])] + self.get_fold_stats(fold))
 
     def __str__(self):
         fold_stats = [self.get_fold_stats(fold)
                       for cat in self.record
                       for fold in self.record[cat]]
-        [ap, rap, p_at_10, rp_at_10, p_at_100, rp_at_100] = np.array(
-            fold_stats).mean(axis=0).tolist()
-        return '(AUPR %.3f %.3f) (P@10 %.3f %.3f) (P@100 %.3f %.3f)' % (
-            ap, rap, p_at_10, rp_at_10, p_at_100, rp_at_100)
+        return '(AUPR %.3f %.3f) (P@10 %.3f %.3f) (P@100 %.3f %.3f) (MRR %.3f %.3f)' % tuple(
+            np.array(fold_stats).mean(axis=0).tolist())
 
 
 def setup():
@@ -304,6 +304,15 @@ def diverse_categories():
     pdb.set_trace()
 
 
+def data_stats():
+    (url_mention, TM, E, cat_folds, cat2url, performance_aggregator) = setup()
+    print 'Total Number of entities', len(E)
+    print 'Total Number of Categories', len(cat2url)
+    print 'Total Number of URLs', len(set(rasengan.flatten(cat2url.itervalues())))
+    print 'Total Number of mentions', sum(len(url_mention[e]) for e in E)
+    return
+
+
 def eval():
     (url_mention, TM, E, cat_folds, cat2url, performance_aggregator) = setup()
     # rasengan.warn('Using Reduced number of categories and folds')
@@ -355,7 +364,12 @@ def eval():
 
 
 def read_report(report_pkl_fn):
-    print pkl.load(open(report_pkl_fn))
+    perf_obj = pkl.load(open(report_pkl_fn))
+    try:
+        print perf_obj.args.ngram_occurrence
+    except AttributeError:
+        pass
+    print perf_obj
     return
 
 
@@ -376,6 +390,7 @@ def main():
     arg_parser.add_argument('--single_person', default=None, type=str)
     arg_parser.add_argument('--diverse_categories', default=None, type=int)
     arg_parser.add_argument('--ngram_occurrence', default=0.7, type=float)
+    arg_parser.add_argument('--data_stats', default=0, type=int)
     args = arg_parser.parse_args()
     with rasengan.warn_ctm('Using Rasengan Tokenizer'):
         # TOKENIZER = lambda x: x.split()
@@ -387,6 +402,9 @@ def main():
         return
     if args.diverse_categories:
         diverse_categories()
+        return
+    if args.data_stats:
+        data_stats()
         return
     read_report(args.report_pkl_fn)
 
