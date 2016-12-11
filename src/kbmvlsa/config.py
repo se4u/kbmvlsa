b87ed6e8e88ddc3c6c246463f03934f5cd4a834e
Pushpendre Rastogi <pushpendre@gmail.com>
2016-12-11 16:12:46 -0500

    added script for doing experiments with galago and with my own python implementation

--- src/kbmvlsa/config.py
+++ src/kbmvlsa/config.py
@@ -4,9 +4,9 @@
 | Description :
 | Author      : Pushpendre Rastogi
 | Created     : Thu Dec  1 20:49:44 2016 (-0500)
-| Last-Updated: Fri Dec  9 04:17:09 2016 (-0500)
+| Last-Updated: Sun Dec 11 15:15:08 2016 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 13
+|     Update #: 21
 '''
 import os
 from os import path
@@ -18,11 +18,25 @@ QUERY_FN = path.join(DBPEDIA_TEST_DATA_DIR, 'queries.txt')
 QRELS_FN = path.join(DBPEDIA_TEST_DATA_DIR, 'qrels.txt')
 
 TREC_WEB_DATA_DIR = path.join(RES_BASE, 'data/chen-xiong-EntityRankData')
+# Some quick facts about dbpedia.trecweb.zip
+# 1. zipout ~/data/chen-xiong-EntityRankData/dbpedia.trecweb.zip | fgrep -c '</DOC>'
+#    takes almost 8 minutes and shows that there are 9.5M entries.
+#    Each record contributes 22 lines, which means that there are 209M lines
+#    in the buffer. (actually 210M  %)
+# 2. Reading these 9.5 million entries in python takes a long time.
+#    It takes 23 min to even read 63M lines.
+#    Without even doing any updates, this means that even without counting
+#    anything, just reading the files in python will take 76.3 minutes.
+# 3. When I add processing then it takes 8 min to read 4.7M lines, which means
+#    processing the file will take 526 minutes = 9hr.
+#    And after 9 hour, I will start the process of pickling and writing to file.
+#    And this will take a lot of memory as well.
 TREC_WEB_DBPEDIA_ZIP = path.join(TREC_WEB_DATA_DIR, 'dbpedia.trecweb.zip')
-TREC_WEB_DBPEDIA_PKL = path.join(TREC_WEB_DATA_DIR, 'dbpedia.trecweb.pkl')
+TREC_WEB_DBPEDIA_GZ = path.join(TREC_WEB_DATA_DIR, 'dbpedia.trecweb.gz')
+TREC_WEB_DBPEDIA_PFX = path.join(
+    RES_BASE, 'export/kbmvlsa_py/dbpedia.trecweb.python', 'py')
 TREC_WEB_CATEGORIES = ['names', 'category', 'attributes', 'SimEn', 'RelEn']
 RANK_SVM_INEX_LD = path.join(TREC_WEB_DATA_DIR, 'RankSVM_INEX-LD.run')
-
 MVLSA_EMB_PKL_FN = path.join(
     RES_BASE, 'data/embedding/mvlsa/combined_embedding_0.emb.pkl')
 DBPEDIA_PFXLEN = len('http://dbpedia.org/resource/')
--- src/kbmvlsa/util_trecweb_parser.py
+++ src/kbmvlsa/util_trecweb_parser.py
@@ -5,56 +5,20 @@
 | Description : Parse the trecweb format files provided by Chen and Xiong
 | Author      : Pushpendre Rastogi
 | Created     : Thu Dec  8 19:39:09 2016 (-0500)
-| Last-Updated: Fri Dec  9 12:44:32 2016 (-0500)
+| Last-Updated: Sun Dec 11 04:45:16 2016 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 71
+|     Update #: 160
 '''
 import xml.sax
 import config
 import sys
 import itertools
-import zipfile
-import cPickle as pickle
-from rasengan import TokenMapper, PUNCT_CHAR, REGEX_SPECIAL_CHAR, ConstantList
-import re
-import scipy
+from rasengan import tictoc, StreamingArrayMaker, print_proc_info
+
 TOPLEVEL_ELEM_NAME = 'XML'
 ROW_ELEM_NAME = 'DOC'
 DOCID_FIELD = 'DOCHDR'
 ARRAY_MAKER = None
-PUNCT_MATCH_REGEX = re.compile(
-    '([%s])'%(''.join(
-        ('\\%s'%e if e in REGEX_SPECIAL_CHAR else e)
-        for e
-        in PUNCT_CHAR)))
-# BUFFER_END_REGEX = re.compile('^.*(XML|DOC|DOCNO|DOCHDR|names|category|attributes|SimEn|RelEn)>$')
-
-class StreamingArrayMaker(object):
-    def __init__(self, ):
-        self.arr_list = [([], [])
-                         for e
-                         in config.TREC_WEB_CATEGORIES]
-        self.ent_map = TokenMapper()
-        self.tm_list = [TokenMapper() for e in config.TREC_WEB_CATEGORIES]
-        return
-
-    def tokenize(self, s):
-        return [e for e in re.split(PUNCT_MATCH_REGEX, s) if e != '']
-
-    def process(self, docid, cat_data):
-        doc_int = self.ent_map([docid])[0]
-        for i, cat in enumerate(config.TREC_WEB_CATEGORIES):
-            data = self.tm_list[i](self.tokenize(cat_data[i]))
-            self.arr_list[i][0].append(doc_int)
-            self.arr_list[i][1].extend(data)
-        return
-
-    def finalize(self):
-        for i in range(len(config.TREC_WEB_CATEGORIES)):
-            data = ConstantList(1, len(self.arr_list[i][0]))
-            self.arr_list[i] = scipy.sparse.coo_matrix(
-                (data, self.arr_list[i]))
-
 
 class StreamHandler(xml.sax.handler.ContentHandler):
     ''' Sax API based XML parser based on the code at
@@ -79,10 +43,7 @@ class StreamHandler(xml.sax.handler.ContentHandler):
             cat_data = [''.join(self.lastEntry[cat]).strip()
                         for cat
                         in config.TREC_WEB_CATEGORIES]
-            # -------------------------------------------------- #
-            # We call ARRAY_MAKER.process to update the matrix . #
-            # -------------------------------------------------- #
-            # ARRAY_MAKER.process(docid, cat_data)
+            ARRAY_MAKER.update_1(docid, cat_data)
             self.lastEntry = None
         elif name == TOPLEVEL_ELEM_NAME:
             raise StopIteration
@@ -94,20 +55,31 @@ class StreamHandler(xml.sax.handler.ContentHandler):
         return
 
 
-if __name__ == '__main__':
-    # use default ``xml.sax.expatreader``
+def main(dbpedia_fn):
+    print 'Will save output to', config.TREC_WEB_DBPEDIA_PFX
     parser = xml.sax.make_parser()
     parser.setContentHandler(StreamHandler())
-    ARRAY_MAKER = StreamingArrayMaker()
-    dbpedia_file = zipfile.ZipFile(config.TREC_WEB_DBPEDIA_ZIP)\
-                          .open('dbpedia.trecweb')
+    global ARRAY_MAKER
+    with tictoc('Initializing Array Maker'):
+        ARRAY_MAKER = StreamingArrayMaker(config.TREC_WEB_CATEGORIES,
+                                          default_shape=(10000000, 100000000))
+    if dbpedia_fn.endswith('zip'):
+        import zipfile
+        dbpedia_file = zipfile.ZipFile(dbpedia_fn)\
+                              .open('dbpedia.trecweb')
+    elif dbpedia_fn.endswith('gz'):
+        import gzip
+        dbpedia_file = gzip.open(dbpedia_fn)
+    counter = 0
     for buffer_ in itertools.chain(['<XML>'], iter(dbpedia_file), ['</XML>']):
         if buffer_:
+            counter += 1
+            if counter%100000 == 0:
+                print counter
             try:
-                buffer_ = buffer_.replace('&', '&amp;')
-                if not buffer_.endswith('>'):
+                buffer_ = buffer_.replace('&', '&amp;').strip()
+                if not buffer_.endswith('>'):  # len(buffer_) > 100
                     buffer_ = buffer_.replace('<', '&lt;')
-                # print buffer_
                 parser.feed(buffer_)
             except StopIteration:
                 break
@@ -115,9 +87,12 @@ if __name__ == '__main__':
                 print >> sys.stderr, 'Error:'
                 print >> sys.stderr, buffer_
                 break
-    import pdb
-    pdb.set_trace()
-    ARRAY_MAKER.finalize()
-    with open(config.TREC_WEB_DBPEDIA_PKL, 'wb') as f:
-        pickle.dump(ARRAY_MAKER, f, protocol=-1)
-    pass
+    with tictoc('Finalizing'):
+        ARRAY_MAKER.finalize()
+    with tictoc('Saving'):
+        ARRAY_MAKER.save_to_pfx(config.TREC_WEB_DBPEDIA_PFX)
+    return
+
+if __name__ == '__main__':
+    print_proc_info()
+    main(config.TREC_WEB_DBPEDIA_GZ)
